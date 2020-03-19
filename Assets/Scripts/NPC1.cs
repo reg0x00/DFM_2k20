d33f37c8b930a9f0f9cb00efeb6abf7bd5b16e37
reg0x00 <reg0x00@gmail.com>
2020-03-20 00:08:03 +0200

    Прикостылено

--- Assets/Scripts/NPC1.cs
+++ Assets/Scripts/NPC1.cs
@@ -7,14 +7,18 @@ public class NPC1 : MonoBehaviour
 {
     public Text NPC1_txt_out;
     public float time_to_print_dlg;
-    string d1 = "- Где ваша \n карточка?\n- ...";
-    string d2 = "- Где ваша \n карточка?\n- вот!\n- проходите";
+    public float print_delay;
+    float print_delay_timer;
+    const string d1 = "- Где ваша \n карточка?\n- ... ";
+    const string d2 = "- Где ваша \n карточка?\n- вот!\n- проходите ";
+    string out_str="";
     float TTD_timer = 0;
     bool pass = false;
     Animator animator;
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        print_delay_timer = print_delay;
     }
     private void FixedUpdate()
     {
@@ -22,20 +26,24 @@ public class NPC1 : MonoBehaviour
         {
             if (GameObject.FindGameObjectsWithTag(Collectibles_ctl.instance.ID_tag).Length == 0)
             {
-                NPC1_txt_out.text = d2;
+                //NPC1_txt_out.text = d2;
+                print_text(d2);
                 animator.SetBool("Pass",true);
                 pass = true;
                 GetComponent<BoxCollider2D>().enabled = false;
             }
             else
             {
-                NPC1_txt_out.text = d1;
+                //NPC1_txt_out.text = d1;
+                print_text(d1);
             }
             TTD_timer -= Time.fixedDeltaTime;
         }
         else
         {
-            NPC1_txt_out.text = "";
+            //NPC1_txt_out.text = "";
+            print_text("");
+            
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
@@ -46,6 +54,29 @@ public class NPC1 : MonoBehaviour
             if (!pass)
             {
                 TTD_timer = time_to_print_dlg;
+            }
+        }
+    }
+    private void print_text(string text)
+    {
+        if (text.Length != (1+ out_str.Length))
+        {
+            if (print_delay_timer <= 0)
+            {
+                if (text.Equals(""))
+                {
+                    out_str = "";
+                }
+                else
+                {
+                    out_str = text.Remove(out_str.Length + 1);
+                }
+                NPC1_txt_out.text = out_str;
+                print_delay_timer = print_delay;
+            }
+            else
+            {
+                print_delay_timer -= Time.fixedDeltaTime;
             }
         }
     }
---
+++ Assets/Scripts/TutorText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorText : MonoBehaviour
+{
+    public Animator animator;
+    //private void Awake()
+    //{
+    //    animator = GetComponent<Animator>();
+    //}
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerController_v3 ctl = collision.GetComponent<PlayerController_v3>();
+        if (ctl != null)
+        {
+            animator.SetBool("Active", true);
+        }
+            
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerController_v3 ctl = collision.GetComponent<PlayerController_v3>();
+        if (ctl != null)
+        {
+            animator.SetBool("Active", false);
+        }
+
+    }
+}

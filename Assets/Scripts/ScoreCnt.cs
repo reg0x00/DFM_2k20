c3fce8a0791a9083a37b4f91ca6a20aeee952e10
reg0x00 <reg0x00@gmail.com>
2020-07-10 20:29:44 +0200

    Никнеймы
    Таблицы результатов + менюшки ввода

---
+++ Assets/Scripts/ScoreCnt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreCnt : MonoBehaviour
+{
+    public string PathToSave;//= "/Saves/ScoreTables.bin"    
+
+        Dictionary<string, Dictionary<string, string>> scores;
+        private string SavePath;
+        private BinaryFormatter formatter;
+
+        public bool HaveUname(string uname,string SceneName)
+        {
+        UpdateDict();
+        if (!scores.ContainsKey(SceneName))
+        {
+            scores.Add(SceneName,new Dictionary<string, string>());
+        }
+        return scores[SceneName].ContainsKey(uname);
+        }
+        public void AddName(string uname,string SceneName)
+        {
+            scores[SceneName].Add(uname, "неуд.");
+            SaveDictToFile();
+        }
+        private void SaveDictToFile()
+        {
+            Stream stream = new FileStream(SavePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, scores);
+            stream.Close();
+        }
+        private void UpdateDict()
+        {
+            Stream stream = new FileStream(SavePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None);
+            if(stream.Length != 0) { 
+            scores = (Dictionary<string, Dictionary<string, string>>)formatter.Deserialize(stream);
+            }
+            else
+            {
+                scores = new Dictionary<string, Dictionary<string, string>>();
+            }
+            stream.Close();
+        }
+     
+    // Start is called before the first frame update
+    void Awake()
+    {
+        formatter = new BinaryFormatter();
+        SavePath = Application.dataPath + PathToSave;
+        UpdateDict();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -8,20 +8,81 @@ public class MainUI : MonoBehaviour
 {
     public GameObject MainCnv;
     public GameObject LvlCnv;
-    private void Start()
+    public GameObject EnterNameCnv;
+    public GameObject EnterNameNext;
+    public GameObject CollisionNameCanv;
+    public Text InputField;
+    ScoreCnt ScoreTable;
+    private string SelectedScene;
+    private string Uname;
+    private void Awake()
     {
-        MainCnv.SetActive(true);
-        LvlCnv.SetActive(false);
-
+        ScoreTable = GameObject.Find("TableCtl").GetComponent<ScoreCnt>();
+        ResetToStartState();
+    }
+    private void Update()
+    {
+        if (LvlCnv.activeSelf && Input.GetKeyDown("escape"))
+        {
+            OnBackToMnuClick();
+        }
+        if (!MainCnv.activeSelf && !LvlCnv.activeSelf && EnterNameCnv.activeSelf && Input.GetKeyDown("escape"))
+        {
+            BackToLvls();
+        }
+        if (EnterNameCnv.activeSelf)
+        {
+            GetInputFromEnterName();
+        }
+        if(CollisionNameCanv.activeSelf && Input.GetKeyDown("escape"))
+        {
+            BackInNameFromCollisionWarn();
+        }
+    }
+    private void BackToLvls()
+    {
+        MainCnv.SetActive(false);
+        LvlCnv.SetActive(true);
+        EnterNameCnv.SetActive(false);
+    }
+    private void OnBackToMnuClick()
+    {
+        ResetToStartState();
     }
     public void OnPlayClick()
     {
-        MainCnv.SetActive(!MainCnv.activeSelf);
-        LvlCnv.SetActive(!LvlCnv.activeSelf);
+        MainCnv.SetActive(false);
+        LvlCnv.SetActive(true);
     }
-    public void LoadLvl(string scene)
+    public void OnLoadLvl(string scene)
     {
-        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        SelectedScene = scene;
+        LvlCnv.SetActive(false);
+        EnterNameCnv.SetActive(true);
+        EnterNameNext.SetActive(false);
+    }
+    public void CheckName()
+    {
+        Uname = InputField.text;
+        if (!ScoreTable.HaveUname(Uname, SelectedScene))
+        {
+            ScoreTable.AddName(Uname, SelectedScene);            
+            LoadLvl();
+        }
+        else
+        {
+            EnterNameCnv.SetActive(false);
+            CollisionNameCanv.SetActive(true);
+        }
+    }
+    public void LoadLvl()
+    {
+        SceneManager.LoadScene(SelectedScene, LoadSceneMode.Single);
+    }
+    public void BackInNameFromCollisionWarn()
+    {
+        CollisionNameCanv.SetActive(false);
+        EnterNameCnv.SetActive(true);
     }
     public void OnOptionsClick()
     {
@@ -31,5 +92,24 @@ public class MainUI : MonoBehaviour
     {
         UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
+    }
+    private void ResetToStartState()
+    {
+        MainCnv.SetActive(true);
+        LvlCnv.SetActive(false);
+        EnterNameCnv.SetActive(false);
+        EnterNameNext.SetActive(false);
+        CollisionNameCanv.SetActive(false);
+    }
+    public void GetInputFromEnterName()
+    {
+        if(InputField.text.Length == 3)
+        {
+            EnterNameNext.SetActive(true);
+        }
+        else
+        {
+            EnterNameNext.SetActive(false);
+        }
     }
 }

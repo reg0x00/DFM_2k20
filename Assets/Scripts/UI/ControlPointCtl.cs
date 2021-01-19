599ea4032e5dd4b387b89091477caa27f39122df
reg0x00 <reg0x00@gmail.com>
2021-01-19 12:19:12 +0200

    ?

---
+++ Assets/Scripts/UI/ControlPointCtl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPointCtl : MonoBehaviour
+{
+    ScoreCnt ScoreTable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreTable = GameObject.Find("TableCtl").GetComponent<ScoreCnt>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.SetActive(false);
+    }
+}

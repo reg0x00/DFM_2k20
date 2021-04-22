7352137b90b2f700b619dc84b9de7cb7f6b95fa0
reg0x00 <reg0x00@gmail.com>
2021-04-22 17:47:39 +0200

    gravi test

--- Assets/Scripts/GravChangeColl.cs
+++ Assets/Scripts/GravChangeColl.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GravChangeColl : MonoBehaviour
 {
     private GameObject PlayerGO;
     public float gravity;
+    public float Y_atten;
+    public float jump_height;
     private float orig_gravity;
+    private float orig_Y_atten;
+    private float orig_jump_height;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +18,27 @@ public class GravChangeColl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerController_v3>())
         {
             orig_gravity = PlayerGO.GetComponent<PlayerController_v3>().gravity;
+            orig_jump_height = PlayerGO.GetComponent<PlayerController_v3>().jump_height;
+            orig_Y_atten = PlayerGO.GetComponent<PlayerController_v3>().y_speed_attenuation_by_time;
             PlayerGO.GetComponent<PlayerController_v3>().gravity = gravity;
+            PlayerGO.GetComponent<PlayerController_v3>().y_speed_attenuation_by_time = Y_atten;
+            PlayerGO.GetComponent<PlayerController_v3>().jump_height = jump_height;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerController_v3>())
+        {
             PlayerGO.GetComponent<PlayerController_v3>().gravity = orig_gravity;
+            PlayerGO.GetComponent<PlayerController_v3>().y_speed_attenuation_by_time = orig_Y_atten;
+            PlayerGO.GetComponent<PlayerController_v3>().jump_height = orig_jump_height;
+        }
     }
 }
